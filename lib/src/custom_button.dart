5b4a28c3e95b8a7cfc90e7340a239564d64bbcc2
Satriahrl_ <satriaherlambang22@gmail.com>
2026-05-15 18:02:43 +0700

    feat: custom button
    variable & widget

--- lib/src/custom_button.dart
+++ lib/src/custom_button.dart
@@ -1,10 +1,35 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  final String label;
+  final VoidCallback onPressed;
+  final Color color;
+  final BorderRadius borderRadius;
+
+  const CustomButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+    this.color = Colors.blueAccent,
+    this.borderRadius = const BorderRadius.all(Radius.circular(8)),
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        shape: RoundedRectangleBorder(
+          borderRadius: borderRadius,
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style:
+            const TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+      ),
+    );
   }
 }
